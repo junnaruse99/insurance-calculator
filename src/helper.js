14d9a3d96ed9dc6e7198f9a35ebb07a90c808257
Jun Naruse <jun.naruse@pucp.edu.pe>
2020-12-26 19:40:28 -0500

    Add a function for insurance by brand and plan
    getInsuranceByBrand is a function that recieves what kind of brand, and with that, calculates the insurance.
    getInsuranceByPlan is the same, but applies the increment according to the type of plan

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,30 @@
-export function getYearDifference(year) {
-    return new Date().getFullYear() - year;
+// Get insurance by year, estimate 3% of decrease of each year of difference from today
+export function getInsuranceByYear(year, insurance) {
+    const difference = new Date().getFullYear() - year
+    return insurance - (( difference * 3 ) * insurance) / 100;;
+}
+
+export function getInsuranceByBrand(brand, insurance) {
+
+    switch(brand) {
+        case 'european':
+            return insurance*1.30;
+        case 'american':
+            return insurance*1.15;
+        case 'asian':
+            return insurance*1.05;
+        default:
+            return insurance;
+    }
+}
+
+export function getInsuranceByPlan(plan, insurance) {
+    switch(plan) {
+        case 'basic':
+            return insurance*1.20;
+        case 'complete':
+            return insurance*1.5;
+        default:
+            return insurance;
+    }
 }
